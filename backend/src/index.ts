a20ff1e9b0ee59a7f66b9831649d543718b7d13a
Seth Burns <seth@sethburns.com>
2023-08-16 13:36:06 -0700

    Setup knex, pg, and .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
 console.log('Hello World!!')
+import knex from 'knex'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve('..', '.env') })
+const pg = knex({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    port: 5432,
+    user: process.env.PG_USER,
+    database: 'nomnom',
+    password: process.env.PG_PASSWORD,
+    // ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
+  }
+});
+
+const result = pg('ingredients')
+  .first()
+  .then(res => console.log(res))
