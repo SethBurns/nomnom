637f86063328091d0d9aecaaa961da757a74d0c6
Seth Burns <seth@sethburns.com>
2023-08-23 22:23:51 -0700

    Fix type

--- frontend/src/components/Ingredients/Ingredients.tsx
+++ frontend/src/components/Ingredients/Ingredients.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 interface Props {
   ingredients: Array<Ingredient>;
-  setIngredients: Function;
+  setIngredients: (ingredients: Array<Ingredient>) => void;
 }
 
 export function Ingredients({ ingredients, setIngredients }: Props) {
