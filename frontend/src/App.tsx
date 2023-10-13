041a9d6518e3b9fc22a6f53feaecdedcd161dcaa
Trevor Scheer <trevor.scheer@gmail.com>
2023-10-13 11:04:48 -0700

    remove reference to missing App.css file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import './App.css';
 import { Ingredients } from './components/Ingredients/Ingredients';
 import { AddIngredientForm } from './components/AddIngredientForm/AddIngredientForm';
 import {
